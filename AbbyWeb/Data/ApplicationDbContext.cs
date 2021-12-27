c530a945406cc677e82100725073e89ea4b15fc7
Precious Nwakama <donprecious97@yahoo.com>
2021-12-27 18:22:35 +0800

    Add project files.

---
+++ AbbyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using AbbyWeb.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbbyWeb.Data
+{
+    //Defined inside the EntityFrameworkCore to access and manage the database
+    public class ApplicationDbContext: DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        //create DbSet for all the table we have in our database
+        //Database name is Category
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ AbbyWeb/Model/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbbyWeb.Model
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Display(Name="Display Name")]
+        [Range(1,100,ErrorMessage = "Display order must be in range of 1-100!!!")]
+        public int DisplayOrder { get; set; }
+    }
+}
